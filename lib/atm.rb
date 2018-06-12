d3ae9aa375ca4863fcd48ab4709b10786959d929
Siianko <tsiyanko@gmail.com>
2018-06-12 15:19:22 +0200

    Test happy and sad pathes, implement withdraw method

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,6 +1,18 @@
 class ATM
-attr_accessor :funds
-def initialize
-    @funds = 1000
+    attr_accessor :funds
+    def initialize
+        @funds = 1000
+    end
 
-end
+    def withdraw(amount, account)
+
+        case
+        when amount > account.balance
+        return
+        else
+        @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount: amount }  
+        end
+    end
+end       
