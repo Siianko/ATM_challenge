959f46e04218822da55f3ddefc5073b3214c18c3
Siianko <tsiyanko@gmail.com>
2018-06-12 17:03:14 +0200

    Perform a check if the amount we try to withdraw is larger then the funds available

--- lib/atm.rb
+++ lib/atm.rb
@@ -8,19 +8,24 @@ class ATM
     def withdraw(amount, account)
         case
         when insufficient_funds_in_account?(amount, account)
-            return
+            { status: false, message: 'insufficient funds', date: Date.today }
+        when insufficient_funds_in_atm?(amount)
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
         else
             perform_transaction(amount, account)  
         end
     end
 
-private
-def insufficient_funds_in_account?(amount, account)
-    amount > account.balance
-end
-def perform_transaction(amount, account)
-    @funds -= amount
-    account.balance = account.balance - amount
-    { status: true, message: 'success', date: Date.today, amount: amount }
-end     
+    private
+    def insufficient_funds_in_account?(amount, account)
+        amount > account.balance
+    end
+    def perform_transaction(amount, account)
+        @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount: amount }
+    end  
+    def insufficient_funds_in_atm?(amount)
+        @funds < amount
+    end   
 end
