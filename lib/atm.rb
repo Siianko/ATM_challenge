2b76c8b0197eb779a5265110ed7be7480ae873e1
Siianko <tsiyanko@gmail.com>
2018-06-15 12:33:24 +0200

    WIP

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,3 +1,4 @@
+require 'date'
 class ATM
     attr_accessor :funds
 
@@ -5,17 +6,22 @@ class ATM
         @funds = 1000
     end
 
-    def withdraw(amount, account)
+    def withdraw(amount, pin_code, account)
         case
         when insufficient_funds_in_account?(amount, account)
             { status: false, message: 'insufficient funds', date: Date.today }
         when insufficient_funds_in_atm?(amount)
             { status: false, message: 'insufficient funds in ATM', date: Date.today }
+        when incorrect_pin?(pin_code, account.pin_code)
+            { status: false, message: 'wrong pin', date: Date.today }
+        when card_expired?(account.exp_date)
+            { status: false, message: 'card expired', date: Date.today }
         else
             perform_transaction(amount, account)  
-        end
+        end   
     end
 
+    
     private
     def insufficient_funds_in_account?(amount, account)
         amount > account.balance
@@ -28,4 +34,10 @@ class ATM
     def insufficient_funds_in_atm?(amount)
         @funds < amount
     end   
+    def incorrect_pin?(pin_code, actual_pin)
+        pin_code != actual_pin
+    end
+    def card_expired?(exp_date)
+        Date.strptime(exp_date, '%m/%y') < Date.today
+    end
 end
